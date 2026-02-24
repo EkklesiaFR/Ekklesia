a8b0c3602626e9681001d258bf734c833409d22c
Bassthet Production <bassthet@gmail.com>
2026-02-24 14:26:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2289029551-f232a",
+  "appId": "1:321075366739:web:de5dcf5885402213d96fa6",
+  "apiKey": "AIzaSyCgMpRH14G15hHy8fLXyBSmci7FKrDv57E",
+  "authDomain": "studio-2289029551-f232a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "321075366739"
+};
